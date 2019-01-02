06cb9da0093ba97679293ad623b8509104733506
Bryant Cabrera <bryantcabrera@Bryants-MacBook-Pro.local>
2019-01-02 11:21:17 -0800

    Creates my Tamagotchi class.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+class Tamagotchi {
+    constructor(name, img) {
+        this.name = name;
+        this.img = img;
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredome = 0;
+        this.age = 0;
+    }
+}
